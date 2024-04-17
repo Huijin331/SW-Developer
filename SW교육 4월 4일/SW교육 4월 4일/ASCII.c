158520422adc4bae2b2663ab3b4b0aae17ed82e8
Huijin331 <jinjin010331@naver.com>
2024-04-17 11:45:42 +0900

    upload C projects

---
+++ SW교육 4월 4일/SW교육 4월 4일/ASCII.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	// 아스키코드 :: 컴퓨터가 문자 정보를 파악할 수 있도록
+	//		문자 정보 - 특정 숫자를 일대일 대응 시킨 코드
+	char alpha;
+	scanf("%c", &alpha);
+
+	// 'A' :: 65, 'a' :: 97, '0' :: 48
+	printf("문자로 표현 :: %c\n", alpha);
+	printf("숫자로 표현 :: %c\n", alpha);
+}
